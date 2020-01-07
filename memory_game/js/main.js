b87639c82ef724d38b8daa0c25727381c9149348
cjstarke <59260296+cjstarke@users.noreply.github.com>
2020-01-07 12:03:27 -0500

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,18 @@
+const cards = [ "queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User fipped " + cardOne);
+
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
